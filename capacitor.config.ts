3ab0afb560757ccd82f8bfad4e8435c50146c356
Federico Lochbaum <federico.lochbaum@gmail.com>
2026-09-25 22:20:01 +0200

    snatchy

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.snatchy.app',
+  appName: 'Snatchy',
+  webDir: 'dist'
+};
+
+export default config;
